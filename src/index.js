4b4b1f41d5f46e5065d07373602987f6d1b81e2f
PARKER\Parker <parkers@mail.ru>
2021-10-14 14:18:10 +0300

    Edit functions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 import "./styles/styles.css";
 import markup from "./js/markup";
-import { handleClick, getCurrentLocation } from "./js/functions";
+import {
+  handleClick,
+  getWeather,
+  getLocalStorage,
+  getMapCity,
+  dataViewer,
+  wrongCity,
+} from "./js/functions";
+import { getCurrentLocation } from "./js/getCurrentLocation";
 
 function startFunction() {
   const app = document.getElementById("app");
   markup(app);
-  getCurrentLocation();
+
+  (async function getCity() {
+    getWeather(await getCurrentLocation());
+    getLocalStorage();
+  })();
+
+  function getHandleClick() {
+    const input = document.getElementById("input");
+    const city = document.getElementById("city");
+    const result = handleClick();
+    if (result !== "error") {
+      getMapCity(result);
+      dataViewer("data", "flex");
+      city.textContent = input.value;
+    } else {
+      wrongCity(true);
+    }
+  }
+
   const button = document.getElementById("button");
-  button.addEventListener("click", handleClick);
+  button.addEventListener("click", getHandleClick);
   document.addEventListener("keyup", ({ key }) => {
     if (key === "Enter") {
-      handleClick();
+      getHandleClick();
     }
   });
 }
--- src/js/functions.js
+++ src/js/functions.js
@@ -1,59 +1,89 @@
-function getSign(value) {
-  if (value > 0) {
-    return `+${value}`;
+export function getSign(value) {
+  let digit;
+  const data = Number(value);
+  if (data > 0) {
+    digit = `+${data}`;
+  } else if (data === 0) {
+    digit = 0;
+  } else {
+    digit = data;
   }
-  return value;
+  return digit;
 }
 
-function dataViewer(block, type) {
+export function dataViewer(block, type) {
   const el = document.getElementById(block);
   el.style.display = type;
 }
 
-function getData(city) {
-  const APIkey = "2f3499bf11f061c267af42038ae180bc";
-  // eslint-disable-next-line max-len
-  return `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`;
+export function getData(city, sourse) {
+  let link;
+  if (sourse) {
+    const APIkey = "AIzaSyB6Y7wf4TCspmqw-ndjkh9yjWk1EW_Eubs";
+    const str = encodeURIComponent(city.trim());
+    // eslint-disable-next-line max-len
+    link = `https://maps.googleapis.com/maps/api/geocode/json?address=${str}&language=en&key=${APIkey}`;
+  } else {
+    const APIkey = "2f3499bf11f061c267af42038ae180bc";
+    // eslint-disable-next-line max-len
+    link = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`;
+  }
+  return link;
 }
 
-function getTemperature(city, id) {
-  const url = getData(city);
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const memory = document.getElementById("memory");
-      const block = document.createElement("div");
-      memory.appendChild(block).setAttribute("class", "block");
-      const param = document.createElement("div");
-      block.appendChild(param).setAttribute("id", id);
-      block.appendChild(param).setAttribute("class", "label btn");
-      param.innerHTML = city;
-      const prop = document.createElement("div");
-      block.appendChild(prop).setAttribute("class", "value");
-      if (data.cod === 200) {
-        prop.innerHTML = getSign(data.main.temp.toFixed());
-      } else {
-        prop.innerHTML = "data not found";
-      }
-      memory.style.display = "block";
-    });
+export function wrongCity(value) {
+  const input = document.getElementById("input");
+  const data = document.getElementById("data");
+  if (value) {
+    input.classList.add("wrong");
+    dataViewer("plugWeather", "flex");
+    dataViewer("weather", "none");
+    dataViewer("cityBlock", "none");
+    dataViewer("map", "none");
+    dataViewer("plugImage", "flex");
+    data.classList.add("empty");
+  } else {
+    input.classList.remove("wrong");
+    dataViewer("weather", "block");
+    dataViewer("cityBlock", "flex");
+    dataViewer("plugWeather", "none");
+    dataViewer("map", "block");
+    dataViewer("plugImage", "none");
+    data.classList.remove("empty");
+  }
 }
 
-function getMap(latitude, longitude) {
+export async function getTemperature(city) {
+  try {
+    const url = getData(city, false);
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.cod === 200) {
+      return getSign(data.main.temp.toFixed());
+    }
+    return "data not found";
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    console.log(error.response);
+    return false;
+  }
+}
+
+export function getMap(latitude, longitude) {
   const myLatLng = { lat: latitude, lng: longitude };
-  const map = new google.maps.Map(document.getElementById("map"), {
+  const map = new window.google.maps.Map(document.getElementById("map"), {
     zoom: 12,
     center: myLatLng,
   });
   // eslint-disable-next-line no-new
-  new google.maps.Marker({
+  new window.google.maps.Marker({
     position: myLatLng,
     map,
     title: "Your location",
   });
 }
 
-function dateConvert(value) {
+export function dateConvert(value) {
   const hour = Math.trunc(value);
   let min = 60 * (value - hour);
   min = min.toFixed();
@@ -63,66 +93,49 @@ function dateConvert(value) {
   return `${hour}:${min}`;
 }
 
-function timeTranslater(value, delim, fixed, minutes) {
-  if (minutes) {
-    return (value * delim).toFixed(fixed);
-  }
+export function timeTranslater(value, delim, fixed) {
   return (value / delim).toFixed(fixed);
 }
 
-function getLengthDay(sunset, sunrise) {
-  const lengthDay = timeTranslater(sunset - sunrise, 3600, 2, false);
-  const hours = Math.floor(lengthDay);
-  const mins = timeTranslater(lengthDay - hours, 60, 0, true);
-  return `${hours}:${mins}`;
-}
-
-function getPressure(value) {
+export function getPressure(value) {
   return (value * 0.750064).toFixed(0);
 }
 
-function getTimeStamp() {
-  const now = new Date();
-  const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  const d = startOfDay / 1000;
-  return d;
+export function getTimeStamp(value, timezone) {
+  const date = new Date(value * 1000);
+  const srtDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  return Date.parse(srtDay) / 1000 - timezone;
 }
 
-function getDirectionWind(deg) {
+export function getDirectionWind(value) {
   let wind;
-  switch (deg.toFixed()) {
-    case deg > 338 && deg <= 23:
-      wind = "N";
-      break;
-    case deg > 23 && deg <= 68:
-      wind = "NE";
-      break;
-    case deg > 68 && deg <= 113:
-      wind = "E";
-      break;
-    case deg > 113 && deg <= 158:
-      wind = "SE";
-      break;
-    case deg > 158 && deg <= 203:
-      wind = "S";
-      break;
-    case deg > 203 && deg <= 248:
-      wind = "SW";
-      break;
-    case deg > 248 && deg <= 293:
-      wind = "W";
-      break;
-    default:
-      wind = "NW";
+  const deg = Number(value).toFixed();
+  if ((deg > 338 && deg <= 360) || (deg >= 0 && deg <= 23)) {
+    wind = "N";
+  } else if (deg > 23 && deg <= 68) {
+    wind = "NE";
+  } else if (deg > 68 && deg <= 113) {
+    wind = "E";
+  } else if (deg > 113 && deg <= 158) {
+    wind = "SE";
+  } else if (deg > 158 && deg <= 203) {
+    wind = "S";
+  } else if (deg > 203 && deg <= 248) {
+    wind = "SW";
+  } else if (deg > 248 && deg <= 293) {
+    wind = "W";
+  } else if (deg > 293 && deg <= 338) {
+    wind = "NW";
   }
   return wind;
 }
 
-export function getWeather(city) {
-  const url = getData(city);
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
+export async function getWeather(city) {
+  const url = getData(city, false);
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.cod === 200) {
       document.getElementById("city").innerHTML = data.name;
       // eslint-disable-next-line max-len
       const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
@@ -148,19 +161,34 @@ export function getWeather(city) {
       );
       document.getElementById("humidity").innerHTML = data.main.humidity;
       document.getElementById("sunrise").innerHTML = dateConvert(
-        timeTranslater(data.sys.sunrise - getTimeStamp(), 3600, 2, false)
+        timeTranslater(
+          data.sys.sunrise - getTimeStamp(data.sys.sunrise, data.timezone),
+          3600,
+          2
+        )
       );
       document.getElementById("sunset").innerHTML = dateConvert(
-        timeTranslater(data.sys.sunset - getTimeStamp(), 3600, 2, false)
+        timeTranslater(
+          data.sys.sunset - getTimeStamp(data.sys.sunset, data.timezone),
+          3600,
+          2
+        )
       );
-      document.getElementById("lengthDay").innerHTML = getLengthDay(
-        data.sys.sunset,
-        data.sys.sunrise
+      document.getElementById("lengthDay").innerHTML = dateConvert(
+        timeTranslater(data.sys.sunset - data.sys.sunrise, 3600, 2)
       );
-
       getMap(data.coord.lat, data.coord.lon);
-    });
-  dataViewer("data", "flex");
+      dataViewer("data", "flex");
+      return true;
+    }
+    wrongCity(true);
+    dataViewer("data", "flex");
+    return false;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    console.log(error.response);
+    return false;
+  }
 }
 
 function getClicker() {
@@ -174,7 +202,22 @@ function getClicker() {
   }, 2000);
 }
 
-function getLocalStorage() {
+function getWeatherCities(city, temp, id) {
+  const memory = document.getElementById("memory");
+  const block = document.createElement("div");
+  memory.appendChild(block).setAttribute("id", city);
+  memory.appendChild(block).setAttribute("class", "block");
+  const param = document.createElement("div");
+  block.appendChild(param).setAttribute("id", id);
+  block.appendChild(param).setAttribute("class", "label btn");
+  param.innerHTML = city;
+  const prop = document.createElement("div");
+  block.appendChild(prop).setAttribute("class", "value");
+  prop.innerHTML = temp;
+  memory.style.display = "block";
+}
+
+export function getLocalStorage() {
   const localStor = localStorage.weatherForecast;
   if (typeof localStor === "undefined") {
     dataViewer("memory", "none");
@@ -186,7 +229,10 @@ function getLocalStorage() {
     }
     const arr = localStor.split(",");
     for (let i = 0; i < arr.length; i += 1) {
-      getTemperature(arr[i], i);
+      (async function getTemp() {
+        const temp = await getTemperature(arr[i]);
+        getWeatherCities(arr[i], temp, i);
+      })();
     }
     dataViewer("memory", "block");
     dataViewer("plugList", "none");
@@ -194,39 +240,7 @@ function getLocalStorage() {
   }
 }
 
-export function getCurrentLocation() {
-  const location = "http://ip-api.com/json/";
-  fetch(location)
-    .then((response) => response.json())
-    .then((data) => {
-      getWeather(data.city);
-      getLocalStorage();
-    });
-}
-
-function wrongCity(value) {
-  const input = document.getElementById("input");
-  const data = document.getElementById("data");
-  if (value) {
-    input.classList.add("wrong");
-    dataViewer("plugWeather", "flex");
-    dataViewer("weather", "none");
-    dataViewer("cityBlock", "none");
-    dataViewer("map", "none");
-    dataViewer("plugImage", "flex");
-    data.classList.add("empty");
-  } else {
-    input.classList.remove("wrong");
-    dataViewer("weather", "block");
-    dataViewer("cityBlock", "flex");
-    dataViewer("plugWeather", "none");
-    dataViewer("map", "block");
-    dataViewer("plugImage", "none");
-    data.classList.remove("empty");
-  }
-}
-
-function saveCity(city) {
+export function saveCity(city) {
   const localStor = localStorage.weatherForecast;
   if (typeof localStor === "undefined") {
     localStorage.setItem("weatherForecast", city);
@@ -245,37 +259,33 @@ function saveCity(city) {
   getLocalStorage();
 }
 
-function getMapCity(value) {
-  const key = "AIzaSyB6Y7wf4TCspmqw-ndjkh9yjWk1EW_Eubs";
-  const str = encodeURIComponent(value.trim());
-  // eslint-disable-next-line max-len
-  const location = `https://maps.googleapis.com/maps/api/geocode/json?address=${str}&language=en&key=${key}`;
-  fetch(location)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.status === "OK") {
-        let city = data.results[0].address_components[0].long_name;
-        if (city === "" || Number.isInteger(+city)) {
-          city = value;
-        }
-        getWeather(city);
-        wrongCity(false);
-        saveCity(city);
-      } else {
-        wrongCity(true);
+export async function getMapCity(value) {
+  const location = getData(value, true);
+  try {
+    const response = await fetch(location);
+    const data = await response.json();
+    if (data.status === "OK") {
+      let city = data.results[0].address_components[0].long_name;
+      if (city === "" || Number.isInteger(+city)) {
+        city = value;
       }
-    });
+      getWeather(city);
+      wrongCity(false);
+      saveCity(city);
+    } else {
+      wrongCity(true);
+    }
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    console.log(error.response);
+  }
 }
 
 export function handleClick() {
   const input = document.getElementById("input");
-  const city = document.getElementById("city");
   const regexp = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/gi;
   if (regexp.test(input.value)) {
-    getMapCity(input.value);
-    dataViewer("data", "flex");
-    city.textContent = input.value;
-  } else {
-    wrongCity(true);
+    return input.value;
   }
+  return "error";
 }
