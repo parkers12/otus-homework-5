6a3af555112fa5084233b8bf0721d8653e3fd910
PARKER\Parker <parkers@mail.ru>
2021-10-16 01:12:23 +0300

    Changed  API geo

--- src/js/getCurrentLocation.js
+++ src/js/getCurrentLocation.js
@@ -1,8 +1,8 @@
 export async function getCurrentLocation() {
   try {
-    const response = await fetch("https://ip-api.com/json/");
+    const response = await fetch("https://api.sypexgeo.net/json/");
     const data = await response.json();
-    return data.city;
+    return data.city.name_ru;
   } catch (error) {
     console.log(`Error: ${error.message}`);
     console.log(error.response);
