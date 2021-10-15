a13b81a222150c0efd813f32707cc5272c4f98e7
PARKER\Parker <parkers@mail.ru>
2021-10-15 23:16:39 +0300

    Edit webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: "src/index.js",
+    main: "./src/index.js",
   },
   output: {
     filename: "js/bundle.js",
